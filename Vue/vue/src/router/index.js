ee5087107bc5ff595411e89998b167e2bb7b2a30
wangxuan@rayclear.com <wangxuan@rayclear.com>
2018-09-11 17:59:33 +0800

    Css命名规则之BEM.md

--- Vue/vue/src/router/index.js
+++ Vue/vue/src/router/index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Excel from '@/components/excel'
-import Home from '@/components/Home'
-import Tree from '@/components/tree'
-import Father from '@/components/father'
-import Child from '@/components/child'
-import Index from '@/components/table'
+import Index from '@/views/index/echarts'
 
 Vue.use(Router)
 
@@ -15,26 +10,6 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
-    },
-    {
-      path: '/tree',
-      name: 'tree',
-      component: Tree
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/father',
-      name: 'father',
-      component: Father
-    },
-    {
-      path: '/child',
-      name: 'child',
-      component: Child
     }
   ]
 })
---
+++ Vue/vue/src/views/index/echarts.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <button @click="handle">测试</button>
+      <div id="main" style="width: 600px;height:400px;"></div>
+    </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handle () {
+      const myChart = echarts.init(document.getElementById('main'))
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [20, 20, 20, 10, 10, 20]
+        }]
+      })
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      const myChart = echarts.init(document.getElementById('main'))
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
+    }, 1000)
+  }
+}
+</script>
+
+<style>
+
+</style>
