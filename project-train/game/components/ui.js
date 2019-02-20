1d4ac2b185a7d79f48880381599d489d7efd2405
wangxuan@rayclear.com <wangxuan@rayclear.com>
2019-02-20 21:17:55 +0800

    game

--- project-train/game/components/ui.js
+++ project-train/game/components/ui.js
@@ -1,9 +1,34 @@
 Vue.component('top-bar', {
-  template: `<div class="top-bar">
-    Top bar
+  template: `<div class="top-bar" :class="'player-' + currentPlayerIndex">
+    <div class="player p0">{{ players[0].name }}</div>
+    <div class="turn-counter">
+      <img class="arrow" src="svg/turn.svg" />
+      <div class="turn">Turn {{turn}}</div>
+    </div> 
+    <div class="player p1">{{ players[1].name }}</div>
   </div>`,
   props: ['players', 'currentPlayerIndex', 'turn'],
   created() {
     console.log(this.players);
   },
+});
+
+Vue.component('card', {
+  template: `<div class="card" :class="'type-' + def.type">
+    <div class="title">{{ def.title }}</div>
+    <img class="separator" src="svg/card-separator.svg" />
+    <div class="description">
+      <div v-html="def.description"></div>
+    </div>
+    <div class="note" v-if="def.note">
+      <div v-html="def.note"></div>
+    </div>
+  </div>`,
+  props: ['def'],
+  methods: {
+    handlePlay() {
+      console.log('You played a card!');
+    }
+  },
 })
+
--- project-train/game/main.js
+++ project-train/game/main.js
@@ -2,6 +2,16 @@ new Vue({
   name: 'game',
   el: '#app',
   data: state,
+  computed: {
+    testCard() {
+      return cards.archers;
+    }
+  },
+  methods: {
+    handlePlay() {
+      console.log('are you ok ?');
+    }
+  },
   mounted() {
     console.log(this.$data === state);
   },
